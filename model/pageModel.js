9fcb4621860323bd582a05b2ac41d48bdc20771a
DirtAndGrass <verdant.luke@gmail.com>
2020-03-16 10:11:29 -0400

    editing pages implemented

--- model/pageModel.js
+++ model/pageModel.js
@@ -1,76 +1,64 @@
-const db = require("../config/db");
+const db = require('../config/db');
 
 module.exports = {
-  editPage: async function(
-    pageKey,
-    title,
-    content,
-    shownInMenu,
-    menuOrder
-  ) {
-    if (shownInMenu === undefined) {
-      shownInMenu = false;
-    }
-    if (menuOrder === undefined) {
-      menuOrder = 4;
-    }
+	editPage: async function(pageKey, title, content, shownInMenu, menuOrder) {
+		if (shownInMenu === undefined) {
+			shownInMenu = false;
+		}
+		if (menuOrder === undefined) {
+			menuOrder = 4;
+		}
 
-    let conn = await db.getConnection();
+		let conn = await db.getConnection();
 
-    try {
-      const result = await conn.query(
-        "UPDATE `pages` SET menuOrder=?, title=?, shownInMenu=?, content=? WHERE  pageKey=?;",
-        [menuOrder, title, shownInMenu, content, pageKey]
-      );
-      conn.end();
-      return result;
-    } catch (e) {
-      return { error: e.message };
-    }
-  },
-  addPage: async function(
-    pageKey,
-    title,
-    content,
-    shownInMenu,
-    menuOrder
-  ) {
-    if (shownInMenu === undefined) {
-      shownInMenu = false;
-    }
-    if (menuOrder === undefined) {
-      menuOrder = 4;
-    }
+		try {
+			const result = await conn.query(
+				'UPDATE `pages` SET menuOrder=?, title=?, shownInMenu=?, content=? WHERE pageKey=?;',
+				[menuOrder, title, shownInMenu, content, pageKey]
+			);
+			conn.end();
+			return result;
+		} catch (e) {
+			return { error: e.message };
+		}
+	},
+	addPage: async function(pageKey, title, content, shownInMenu, menuOrder) {
+		if (shownInMenu === undefined) {
+			shownInMenu = false;
+		}
+		if (menuOrder === undefined) {
+			menuOrder = 4;
+		}
 
-    let conn = await db.getConnection();
+		let conn = await db.getConnection();
 
-    try {
-      const result = await conn.query(
-        "INSERT INTO pages (`pageKey`, `title`, `content`,shownInMenu, menuOrder) VALUES (?, ?, ?, ?, ?);",
-        [pageKey, title, content, shownInMenu, menuOrder]
-      );
-      conn.end();
-      return result;
-    } catch (e) {
-      return { error: e.message };
-    }
-  },
-  getPage: async function(key) {
-    //key = tolowercase(trim(key));
-    let conn = await db.getConnection();
-    const row = await conn.query(
-      "SELECT pageKey, title,content FROM pages WHERE pageKey = ?",
-      [key]
-    );
-    conn.end();
-    return row;
-  },
-  getMenu: async function() {
-    let conn = await db.getConnection();
-    const rows = await conn.query(
-      "SELECT pageKey, title FROM pages WHERE shownInMenu = 1 ORDER BY menuOrder"
-    );
-    conn.end();
-    return rows;
-  }
+		try {
+			const result = await conn.query(
+				'INSERT INTO pages (`pageKey`, `title`, `content`,shownInMenu, menuOrder) VALUES (?, ?, ?, ?, ?);',
+				[pageKey, title, content, shownInMenu, menuOrder]
+			);
+			conn.end();
+			return result;
+		} catch (e) {
+			return { error: e.message };
+		}
+	},
+	getPage: async function(key) {
+		//key = tolowercase(trim(key));
+		let conn = await db.getConnection();
+		const row = await conn.query(
+			'SELECT pageKey, title,content, shownInMenu, menuOrder FROM pages WHERE pageKey = ?',
+			[key]
+		);
+		conn.end();
+		return row;
+	},
+	getMenu: async function() {
+		let conn = await db.getConnection();
+		const rows = await conn.query(
+			'SELECT pageKey, title FROM pages WHERE shownInMenu = 1 ORDER BY menuOrder'
+		);
+		conn.end();
+		return rows;
+	}
 };
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -1,93 +1,126 @@
-const router = require("express").Router();
-const pageModel = require("../model/pageModel");
+const router = require('express').Router();
+const pageModel = require('../model/pageModel');
 
 // router.post("/login", async (req, res) => {
 //   console.log(req.body);
 //   res.send("body " + req.body.username);
 // });
 
-router.all("/", async (req, res) => {
-  getPageWithDefault(req, res);
+router.all('/', async (req, res) => {
+	getPageWithDefault(req, res);
 });
 
-router.post("/add/", async (req, res, next) => {
-  if (req.user !== undefined && req.user.auth) {
-    let d = req.body;
-    const result = await pageModel.addPage(
-      d.pageKey,
-      d.title,
-      d.content,
-      d.shownInMenu === "on",
-      d.menuOrder
-    );
+router.post('/edit/', async (req, res, next) => {
+	if (req.user !== undefined && req.user.auth) {
+		let d = req.body;
+		const result = await pageModel.editPage(
+			d.pageKey,
+			d.title,
+			d.content,
+			d.shownInMenu === 'on',
+			d.menuOrder
+		);
 
-    if (result.error !== undefined) {
-      res.render("editedPageView", {
-        page: { title: "Error Adding Page", pageKey: d.pageKey },
-        user: req.user,
-        error: result.error
-      });
-    } else {
-      res.render("editedPageView", {
-        page: { title: d.title, pageKey: d.pageKey },
-        user: req.user,
-        menu: await pageModel.getMenu()
-      });
-    }
-  } else {
-    next();
-  }
+		if (result.error !== undefined) {
+			res.render('editedPageView', {
+				page: { title: 'Error Edit Page', pageKey: d.pageKey },
+				added: false,
+				user: req.user,
+				error: result.error
+			});
+		} else {
+			res.render('editedPageView', {
+				page: { title: d.title, pageKey: d.pageKey },
+				user: req.user,
+				added: false,
+				menu: await pageModel.getMenu()
+			});
+		}
+	} else {
+		next();
+	}
+});
+router.post('/add/', async (req, res, next) => {
+	if (req.user !== undefined && req.user.auth) {
+		let d = req.body;
+		const result = await pageModel.addPage(
+			d.pageKey,
+			d.title,
+			d.content,
+			d.shownInMenu === 'on',
+			d.menuOrder
+		);
+
+		if (result.error !== undefined) {
+			res.render('editedPageView', {
+				page: { title: 'Error Adding Page', pageKey: d.pageKey },
+				user: req.user,
+				added: true,
+				error: result.error
+			});
+		} else {
+			res.render('editedPageView', {
+				page: { title: d.title, pageKey: d.pageKey },
+				user: req.user,
+				added: true,
+				menu: await pageModel.getMenu()
+			});
+		}
+	} else {
+		next();
+	}
 });
 
-router.get("/add/", async (req, res, next) => {
-  if (req.user !== undefined && req.user.auth) {
-    let menu = await pageModel.getMenu();
-    res.render("editPageView", {
-      page: { title: "Add Page" },
-      menu: menu,
-      user: req.user
-    });
-  } else {
-    next();
-  }
+router.get('/add/', async (req, res, next) => {
+	if (req.user !== undefined && req.user.auth) {
+		let menu = await pageModel.getMenu();
+		res.render('editPageView', {
+			page: { title: 'Add Page' },
+			menu: menu,
+			user: req.user
+		});
+	} else {
+		next();
+	}
 });
 
-router.get("/edit/:page", async (req, res, next) => {
-  if (req.user !== undefined && req.user.auth) {
-    let menu = await pageModel.getMenu();
-    res.render("editPageView", {
-      page: { title: "Edit Page", key: req.params.page },
-      menu: menu,
-      user: req.user
-    });
-  } else {
-    next();
-  }
+router.get('/edit/:key', async (req, res, next) => {
+	if (req.user !== undefined && req.user.auth) {
+		let menu = await pageModel.getMenu();
+		let page = await pageModel.getPage(req.params.key);
+		res.render('editPageView', {
+			page: { title: 'Edit Page', data: page[0] },
+			menu: menu,
+			user: req.user
+		});
+	} else {
+		next();
+	}
 });
 
-router.all("/:key", async (req, res) => {
-  getPageWithDefault(req, res);
+router.all('/:key', async (req, res) => {
+	getPageWithDefault(req, res);
 });
 
 async function getPageWithDefault(req, res) {
-  if (req.params.key === undefined) {
-    req.params.key = "home";
-  }
-  let page = await pageModel.getPage(req.params.key);
-  let menu = await pageModel.getMenu();
+	if (req.params.key === undefined) {
+		req.params.key = 'home';
+	}
+	let page = await pageModel.getPage(req.params.key);
+	let menu = await pageModel.getMenu();
 
-  //console.log(menu);
-  if (page[0] !== undefined) {
-    res.render("pageView", {
-      page: page[0],
-      menu: menu,
-      user: req.user
-    });
-  } else {
-    //res.statusMessage = "Page not available";
-    res.status(404);
-    res.render("statusView", { code: 404, status: "Not Found" });
-  }
+	console.log(req.user);
+	if (page[0] !== undefined) {
+		res.render('pageView', {
+			page: page[0],
+			menu: menu,
+			user: req.user
+		});
+	} else {
+		//res.statusMessage = "Page not available";
+		res.status(404);
+		res.render('statusView', { code: 404, status: 'Not Found' });
+	}
 }
 
 module.exports = router;
